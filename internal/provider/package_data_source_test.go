3d9f9fa8b415a1c9eeeb3b443d26de584a519684
Jason Washburn <jason.washburn@gmail.com>
2026-09-12 17:44:10 -0500

    test: remove architecture dependent digest check

--- internal/provider/package_data_source_test.go
+++ internal/provider/package_data_source_test.go
@@ -14,7 +14,6 @@ func TestAccPackageDataSource(t *testing.T) {
 			{
 				Config: providerConfig + `data "zarf_package" "test" {source = "ghcr.io/zarf-dev/packages/dos-games:1.3.0"}`,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.zarf_package.test", "digest", "sha256:e087e4eeca62f9b6263d69ff567b0046c1881e689925c120f4846a5d7b278c1a"),
 					resource.TestCheckResourceAttr("data.zarf_package.test", "metadata.name", "dos-games"),
 					resource.TestCheckResourceAttr("data.zarf_package.test", "metadata.description", "Simple example to load classic DOS games into K8s in the airgap"),
 					resource.TestCheckResourceAttr("data.zarf_package.test", "metadata.version", "1.3.0"),
